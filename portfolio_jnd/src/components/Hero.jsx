e2de5d87b887ab566f5dd4335836d19a67c3c397
jamesnd04 <jamesdankwah12@gmail.com>
2023-05-31 09:51:49 -0400

    <feat> added Navbar

---
+++ portfolio_jnd/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Hero = () => {
+  return (
+    <div className = 'w-full h-screen bg-[#011c38]'>
+
+
+    </div>
+  )
+}
+
+export default Hero
