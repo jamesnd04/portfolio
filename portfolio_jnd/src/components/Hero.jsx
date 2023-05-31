e34a339391eaa60e126d71a5ac80aba5a1783e54
jamesnd04 <jamesdankwah12@gmail.com>
2023-05-31 16:00:40 -0400

    <feat> typing animation

--- portfolio_jnd/src/components/Hero.jsx
+++ portfolio_jnd/src/components/Hero.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from 'react';
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import Typed from 'react-typed'
+
+
 
 const Hero = () => {
-  return (
-    <div className = 'w-full h-screen bg-[#011c38]'>
-
-
-    </div>
-  )
-}
-
-export default Hero
+    return (
+      <div name='home' className='w-full h-screen bg-[#011c38]'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+          <p>Hi, my name is</p>
+          <h1>James Dankwah</h1>
+          <h2>
+            I'm a <span><Typed
+      strings={[
+            "Student",
+            "Developer",
+            "Designer",
+          ]}
+          typeSpeed={75}
+          backSpeed={200}
+          loop
+        /></span>
+          </h2>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Hero;
+  
--- portfolio_jnd/src/components/Navbar.jsx
+++ portfolio_jnd/src/components/Navbar.jsx
@@ -37,10 +37,18 @@ const Navbar = () => {
         </ul>
 
         {/*Social Icons */}
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1] '>
                     <a className='flex justify-between items-center w-full text-gray-300'href = '/'>LinkedIn <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] '>
+                    <a className='flex justify-between items-center w-full text-gray-300'href = '/'>Github <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] '>
+                    <a className='flex justify-between items-center w-full text-gray-300'href = '/'>Resume <BsFillPersonLinesFill size={30}/>
                     </a>
                 </li>
             </ul>
