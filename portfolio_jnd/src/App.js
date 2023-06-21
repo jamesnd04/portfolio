da67cc3d8974751f191441cf682385ae8ffeb3fd
jamesnd04 <jamesdankwah12@gmail.com>
2023-06-21 18:41:33 -0400

    <bottom>

--- portfolio_jnd/src/App.js
+++ portfolio_jnd/src/App.js
@@ -2,8 +2,10 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Bottom from "./components/Bottom"
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
       <Skills/>
       <Projects/>
       <Contact/>
+      <Bottom/>
     </div>
   );
 }
