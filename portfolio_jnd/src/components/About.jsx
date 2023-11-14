0933462d151c4ad46b2aff3330ef80164e36e526
jamesnd04 <jamesdankwah12@gmail.com>
2023-11-14 13:20:15 -0500

    <feat> fixed deployment issue

--- portfolio_jnd/src/components/About.jsx
+++ portfolio_jnd/src/components/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
                 <div></div>
                 <div className='max-w-[1000px] w-full h-[288px] '>
                 <p className='justify-center text-size-adjust'>I am a sophomore at the<p className=' text-[#99badd]'> University of North Carolina at Chapel Hill</p>
-                        where I'm majoring in Computer Science(B.S) with a minor in Data Science and Business <Administration></Administration>. Some relevant courses I've taken include Data Structures and Analysis, Discrete Structures,
+                        where I'm majoring in Computer Science(B.S) with a minor in Data Science and Business Administration. Some relevant courses I've taken include Data Structures and Analysis, Discrete Structures,
                         and Calculus of Functions. At UNC, I'm involved in Black in Tech, Acts to Fellowship, and I serve as the Membership Officer for TechX, which is a software development society.
                         Within computer science, I am most interested in software engineering, numerical analysis, and artificial intelligence.
                         Outside of that, I enjoy playing and watching soccer, going to concerts, and watching film analysis.
